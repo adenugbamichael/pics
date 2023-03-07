d1531dec9d53ec4a794dc4dfe37f30d80b0f7e5e
adenugbamichael <adenugbamicke@gmail.com>
2023-03-07 10:27:33 +0100

    showing form to the user witha touch of style

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const SearchBar = () => {
+  return (
+    <div className='ui segment'>
+      <form className='ui form' action=''>
+        <div className='field'>
+          <label htmlFor=''>Image Search</label>
+          <input type='text' />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SearchBar
