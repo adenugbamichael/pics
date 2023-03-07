116f71fe29bf3c38e1f96764db552c34b65108fc
adenugbamichael <adenugbamicke@gmail.com>
2023-03-07 16:10:20 +0100

    invoking callbacks in children

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,30 @@
 import React from "react"
 
-const SearchBar = () => {
-  return (
-    <div className='ui segment'>
-      <form className='ui form' action=''>
-        <div className='field'>
-          <label htmlFor=''>Image Search</label>
-          <input type='text' />
-        </div>
-      </form>
-    </div>
-  )
+class SearchBar extends React.Component {
+  state = { term: "" }
+
+  onFormSubmit = (event) => {
+    event.preventDefault()
+
+    this.props.onSubmit(this.state.term)
+  }
+
+  render() {
+    return (
+      <div className='ui segment'>
+        <form className='ui form' onSubmit={this.onFormSubmit}>
+          <div className='field'>
+            <label htmlFor=''>Image Search</label>
+            <input
+              type='text'
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default SearchBar
